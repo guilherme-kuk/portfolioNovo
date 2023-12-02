450700ce40913e9b772fb9fd52a23bff8ec5f097
Guilherme Kuk <guilhermekuk@gmail.com>
2023-12-02 19:38:29 -0300

    Remoção de script e ajuste de particles

--- assets/scripts/particles.js
+++ assets/scripts/particles.js
@@ -1,6 +1,6 @@
 particlesJS("particles-js", {
 	particles: {
-		number: { value: 150, density: { enable: true, value_area: 800 } },
+		number: { value: 190, density: { enable: true, value_area: 800 } },
 		color: { value: "#ffffff" },
 		shape: {
 			type: "circle",
